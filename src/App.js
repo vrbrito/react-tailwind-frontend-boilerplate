3b07af5bafd16a9b1af12dbd22e703e362864e3d
Vitor Ramalho <vitor.ramalho@bct.ect.ufrn.br>
2021-05-31 16:30:19 -0300

    Revert "add base pages"
    This reverts commit 48fc4738442a8838af743af04925f6e5abe370b0.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-
-import Dashboard from './Pages/Main/Dashboard'
-import Team from './Pages/Main/Team'
-
-import SignIn from './Pages/Users/SignIn'
-
-import {Switch, Route} from 'react-router-dom';
-
 
 function App() {
   return (
-    <main>
-      <Switch>
-
-        <Route path="/register">
-          <SignIn />
-        </Route>
-
-        <Route path="/login">
-          <SignIn />
-        </Route>
-
-        <Route path="/team">
-          <Team />
-        </Route>
-
-        <Route path="/">
-          <Dashboard />
-        </Route>
-
-      </Switch>
-    </main>
+    <div className="App">
+    </div>
   );
 }
 
